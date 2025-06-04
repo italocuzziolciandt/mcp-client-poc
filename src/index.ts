9e4167ae6b8be404131483ad4f06215f634b2419
Italo Cuzziol Ferreira <italocf@ciandt.com>
2025-06-04 11:47:18 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,185 @@
+import OpenAI from "openai";
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+import readline from "readline/promises";
+import dotenv from "dotenv";
+import { FunctionTool } from "openai/resources/responses/responses.mjs";
+
+dotenv.config(); // load environment variables from .env
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+if (!OPENAI_API_KEY) {
+  throw new Error("OPENAI_API_KEY is not set");
+}
+
+class MCPClient {
+  private mcp: Client;
+  private openai: OpenAI;
+  private transport: StdioClientTransport | null = null;
+  private tools: FunctionTool[] = [];
+
+  constructor() {
+    this.openai = new OpenAI({
+      apiKey: OPENAI_API_KEY,
+    });
+
+    this.mcp = new Client({ name: "mcp-client-cli", version: "1.0.0" });
+  }
+
+  async connectToServer(serverScriptPath: string) {
+    /**
+     * Connect to an MCP server
+     *
+     * @param serverScriptPath - Path to the server script (.py or .js)
+     */
+    try {
+      // Determine script type and appropriate command
+      const isJs = serverScriptPath.endsWith(".js");
+      const isPy = serverScriptPath.endsWith(".py");
+      if (!isJs && !isPy) {
+        throw new Error("Server script must be a .js or .py file");
+      }
+      const command = isPy
+        ? process.platform === "win32"
+          ? "python"
+          : "python3"
+        : process.execPath;
+
+      // Initialize transport and connect to server
+      this.transport = new StdioClientTransport({
+        command,
+        args: [serverScriptPath],
+      });
+
+      this.mcp.connect(this.transport);
+
+      // List available tools
+      const toolsResult = await this.mcp.listTools();
+
+      this.tools = toolsResult.tools.map((tool) => {
+        return {
+          type: "function",
+          strict: true,
+          name: tool.name,
+          description: tool.description,
+          parameters: tool.inputSchema,
+        };
+      });
+
+      console.log(
+        "Connected to server with tools:",
+        this.tools.map(({ name }) => name)
+      );
+    } catch (e) {
+      console.log("Failed to connect to MCP server: ", e);
+      throw e;
+    }
+  }
+
+  async processQuery(query: string) {
+
+    const messages: { role: any, content: any }[] = [
+      {
+        role: "user",
+        content: query
+      },
+    ];
+
+    const response = await this.openai.responses.create({
+      model: "o4-mini",
+      input: messages,
+      tools: this.tools
+    });
+
+    const finalText = [];
+
+    for (const output of response.output) {
+      if (output.type === "message") {
+        finalText.push(response.output_text);
+      } else if (output.type === "function_call") {
+        // Execute tool call
+        const toolName = output.name;
+        const toolArgs = typeof output.arguments === "string"
+          ? JSON.parse(output.arguments) as { [x: string]: unknown }
+          : output.arguments as { [x: string]: unknown } | undefined;
+
+        const result = await this.mcp.callTool({
+          name: toolName,
+          arguments: toolArgs,
+        });
+  
+        finalText.push(
+          `[Calling tool ${toolName} with args ${JSON.stringify(toolArgs)}]`,
+        );
+
+        messages.push({
+          role: "user",
+          content: Array.isArray(result.content) && typeof result.content[0]?.text === "string"
+            ? result.content[0].text
+            : ""
+        });
+
+        const response = await this.openai.responses.create({
+          model: "o4-mini",
+          input: messages,
+        });
+
+        finalText.push(
+          response.output.pop()?.type === "message" ? response.output_text : "",
+        );
+      }
+    }
+
+    return finalText.join("\n");
+  }
+
+  async chatLoop() {
+    /**
+     * Run an interactive chat loop
+     */
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    try {
+      console.log("\nMCP Client Started!");
+      console.log("Type your queries or 'quit' to exit.");
+
+      while (true) {
+        const message = await rl.question("\nQuery: ");
+        if (message.toLowerCase() === "quit") {
+          break;
+        }
+        const response = await this.processQuery(message);
+        console.log("\n" + response);
+      }
+    } finally {
+      rl.close();
+    }
+  }
+
+  async cleanup() {
+    /**
+     * Clean up resources
+     */
+    await this.mcp.close();
+  }
+}
+
+async function main() {
+  if (process.argv.length < 3) {
+    console.log("Usage: node build/index.js <path_to_server_script>");
+    return;
+  }
+  const mcpClient = new MCPClient();
+  try {
+    await mcpClient.connectToServer(process.argv[2]);
+    await mcpClient.chatLoop();
+  } finally {
+    await mcpClient.cleanup();
+    process.exit(0);
+  }
+}
+
+main();
